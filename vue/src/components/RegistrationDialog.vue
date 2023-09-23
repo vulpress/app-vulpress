b965c5f830a5173f50fe51fb216d499062397930
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2023-09-24 00:07:47 +0200

    [#50] hotfix 0.1.1
    - fix mailing service autowiring: prod is expected to run in `mail` profile
    - apply constraints to registration dialog form

--- vue/src/components/RegistrationDialog.vue
+++ vue/src/components/RegistrationDialog.vue
@@ -11,6 +11,25 @@ const username: Ref<string> = ref<string>('');
 const password: Ref<string> = ref<string>('');
 const passwordRepeat: Ref<string> = ref<string>('');
 
+const usernameRules: any[] = [
+  (f: any) => !!f || 'Email is required',
+  (f: any) => {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(f))
+      return true;
+    return 'A valid e-mail address is required';
+  },
+];
+const passwordRules: any[] = [
+  (f: any) => !!f || 'Password is required',
+  (f: any) => f.length >= 8 || 'Password must be at least 8 characters long!',
+];
+const confirmPasswordRules: any[] = [
+  (f: any) => !!f || 'Password must be repeated to confirm it',
+  (f: any) => f === password.value || 'Passwords must match',
+];
+
+const valid = ref<boolean | null>(null);
+
 const loading: Ref<boolean> = ref<boolean>(false);
 
 async function onSubmit() {
@@ -33,18 +52,20 @@ async function onSubmit() {
 <template>
   <v-card class="dialog-card">
     <v-card-title class="dialog-title">Register</v-card-title>
-    <v-form class="dialog-form" @submit.prevent="onSubmit">
+    <v-form fast-fail v-model="valid" class="dialog-form" @submit.prevent="onSubmit">
       <v-text-field
         type="text"
         id="username"
         v-model="username"
-        label="Username"
+        :rules="usernameRules"
+        label="E-mail"
         variant="outlined"
       ></v-text-field>
       <v-text-field
         type="password"
         id="password"
         v-model="password"
+        :rules="passwordRules"
         label="Password"
         variant="outlined"
       ></v-text-field>
@@ -52,12 +73,15 @@ async function onSubmit() {
         type="password"
         id="confirm-password"
         v-model="passwordRepeat"
+        :rules="confirmPasswordRules"
         label="Confirm password"
         variant="outlined"
       ></v-text-field>
       <div class="ui-action-container">
         <v-btn class="ui-action ui-action-disabled" @click="$emit('close')">Cancel</v-btn>
-        <v-btn class="ui-action" color="primary" :loading="loading" type="submit">Register</v-btn>
+        <v-btn class="ui-action" color="primary" :loading="loading" :disabled="!valid" type="submit"
+          >Register</v-btn
+        >
       </div>
     </v-form>
   </v-card>
