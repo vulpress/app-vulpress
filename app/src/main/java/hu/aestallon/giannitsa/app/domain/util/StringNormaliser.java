1113c93b67970812858b8911a0983fbc0c6b097f
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2023-09-23 19:16:46 +0200

    [#24] romanise greek letters and strip articles from special characters

--- app/src/main/java/hu/aestallon/giannitsa/app/domain/util/StringNormaliser.java
+++ app/src/main/java/hu/aestallon/giannitsa/app/domain/util/StringNormaliser.java
@@ -2,8 +2,14 @@ package hu.aestallon.giannitsa.app.domain.util;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 
 public final class StringNormaliser {
+
+  private static final char[] ROMANISED_GREEK_LETTERS = { // 945 - 969
+      'a', 'b', 'g', 'd', 'e', 'z', 'h', 'u', 'i', 'k', 'l', 'm', 'n',
+      'j', 'o', 'p', 'r', 'w', 's', 't', 'y', 'f', 'x', 'c', 'v'
+  };
 
   /**
    * Normalises a string, by setting it to lowercase, replacing whitespaces with hyphens, and
@@ -14,7 +20,18 @@ public final class StringNormaliser {
    * @return the normalised {@code String}
    */
   public static String normalise(String s) {
-    return URLEncoder.encode(s.toLowerCase().replaceAll("\\s", "-"), StandardCharsets.UTF_8);
+    String temp = s.toLowerCase().replaceAll("\\s+", "-");
+    if (!Normalizer.isNormalized(temp, Normalizer.Form.NFKC)) {
+      temp = Normalizer.normalize(temp, Normalizer.Form.NFKC);
+      temp = temp.replaceAll("\\p{M}", "");
+    }
+    char[] arr = temp.toCharArray();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] >= 'α' && arr[i] <= 'ω') {
+        arr[i] = ROMANISED_GREEK_LETTERS[arr[i] - 'α'];
+      }
+    }
+    return new String(arr);
   }
 
   private StringNormaliser() {}
